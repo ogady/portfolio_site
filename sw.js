4656946ff8d870c609b13b33790c191ff84f6f3d
ogady <ogady@users.noreply.github.com>
2020-06-26 15:03:43 +0000

    Automated deployment: Fri Jun 26 15:03:42 UTC 2020 95b043593cac211b4fc32d210fe371c7df374a6a

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "styles.e85c0cb7ccf3d0e995ee.css"
   },
   {
-    "url": "styles-34725f49ff8529cc2145.js"
+    "url": "styles-e07bc6e641637ade6339.js"
   },
   {
     "url": "commons-7620e107adc7ab0a1720.js"
@@ -46,11 +46,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "74de1bce6513fbff5c6a718d3578f382"
+    "revision": "aed72e66b821daede985010fcc552735"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "3e8727dd3c91b608f03c527bc669f63a"
+    "revision": "360501b0e0613c0a1db4310dcfe48912"
   },
   {
     "url": "manifest.webmanifest",
